680bb5b8671ce7d814f93656bb3e8e7293d0acda
Anna Kapitanova <totoshechka19@gmail.com>
2024-11-06 17:34:05 +0300

    файлы конфигурации

---
+++ src/app.tsx
@@ -0,0 +1,16 @@
+import Header from './components/header';
+import Footer from './components/footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2>Main Content</h2>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
