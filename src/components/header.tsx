0cea73d113c35d9a0846ae44bb940de3aa88692e
Anna Kapitanova <totoshechka19@gmail.com>
2024-11-07 13:31:57 +0300

    начала делать header, сделала блок с лого и кнопкой Каталог

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,16 +4,20 @@ import { AppRoute } from '../conts';
 function Header() {
   return (
     <header className="header">
-      <div className="header__container container">
-        <div className="header__block">
+      <div className="header__inner container">
+        <div className="header__block header__block-drop-down-list">
           <Link className="header__logo logo" to={AppRoute.Catalog} aria-label="Переход на главную">
-            Логотип
+            <img className="logo__img" src="/images/logo.svg" alt="Логотип E-Shop" width="40" height="40" />
+            <span className="logo__title">E-shop</span>
           </Link>
+          <button className="header__catalog-btn">
+            <img className="catalog-btn__img" src="/images/catalog-icon.svg" alt="Иконка каталога" width="21" height="21" />
+            <span className="catalog-btn__title">Каталог</span>
+          </button>
         </div>
         <div className="header__block">
-          Кнопка каталог
+          Корзина, контакты
         </div>
-
       </div>
     </header>
   );
