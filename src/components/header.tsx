d8e53d527862b4c06eedbe227fc8422ba75dddbb
Anna Kapitanova <totoshechka19@gmail.com>
2024-11-08 13:07:10 +0300

    вынесла логотип в отдельный компонент

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import Logo from './logo';
 import Contacts from './contacts';
 import { AppRoute } from '../conts';
 
@@ -7,10 +8,7 @@ function Header() {
     <header className="header">
       <div className="header__inner container">
         <div className="header__block header__block-drop-down-list">
-          <Link className="header__logo logo" to={AppRoute.Catalog} aria-label="Переход на главную">
-            <img className="logo__img" src="/images/logo.svg" alt="Логотип E-Shop" width="40" height="40" />
-            <span className="logo__title">E-shop</span>
-          </Link>
+          <Logo />
           <button
             className="header__catalog-btn"
             /*!TODO доделать функционал открытия/закрытия выпадающего списка с категориями товаров*/
