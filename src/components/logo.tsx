290bfe57850788908f125056d183da345e7ecf4d
Anna Kapitanova <totoshechka19@gmail.com>
2025-01-06 00:31:39 +0300

    переделала пути к картинкам на абсолютные

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -8,7 +8,7 @@ type LogoProps = {
 function Logo({ className = '' }: LogoProps) {
   return (
     <Link className={`${className} logo`} to={AppRoute.Catalog} aria-label="Переход на главную">
-      <img className="logo__img" src="images/logo.svg" alt="Логотип E-Shop" width="40" height="40" />
+      <img className="logo__img" src="/images/logo.svg" alt="Логотип E-Shop" width="40" height="40" />
       <span className="logo__title">E-shop</span>
     </Link>
   );
