9716c3c7a0dc7dc2c737fd11918453cb1076c31c
Anna Kapitanova <totoshechka19@gmail.com>
2025-03-26 16:07:41 +0300

    вернула PowerShell-скрипт

--- replace-paths.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Замена путей в файлах JavaScript
-for file in dist/assets/*.js; do
-sed -i 's|/images/|/e-shop/images/|g' "$file"
-done
-
-echo "Пути к изображениям успешно заменены!"
