728aa624fcd7f09d1adc51963e3e7e790795bd72
Anna Kapitanova <totoshechka19@gmail.com>
2025-03-26 16:36:48 +0300

    вернула PowerShell-скрипт

--- replace-paths.ps1
+++ replace-paths.ps1
@@ -1,14 +1,4 @@
-# Создаем подпапку e-shop
-New-Item -ItemType Directory -Path "dist/e-shop" -Force
-
-# Перемещаем все файлы в подпапку e-shop
-Get-ChildItem -Path "dist" | Where-Object { $_.Name -ne "e-shop" } | Move-Item -Destination "dist/e-shop"
-
-# Замена путей в файлах JavaScript и HTML
-Get-ChildItem -Path "dist/e-shop/assets" -Filter *.js | ForEach-Object {
+Get-ChildItem dist/assets/*.js | ForEach-Object {
   (Get-Content $_.FullName) -replace '/images/', '/e-shop/images/' | Set-Content $_.FullName
 }
-
-(Get-Content "dist/e-shop/index.html") -replace '/images/', '/e-shop/images/' | Set-Content "dist/e-shop/index.html"
-
-Write-Host "Файлы успешно перемещены в папку e-shop!"
+Write-Host "Пути к изображениям успешно заменены!"
