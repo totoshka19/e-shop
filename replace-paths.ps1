8d61ddb698e800f0e95ec03bd515e729e37f3235
Anna Kapitanova <totoshechka19@gmail.com>
2025-03-27 11:51:31 +0300

    Fix build issues

--- replace-paths.ps1
+++
@@ -1,4 +0,0 @@
-Get-ChildItem dist/assets/*.js | ForEach-Object {
-   (Get-Content $_.FullName) -replace '/images/', '/e-shop/images/' | Set-Content $_.FullName
- }
- Write-Host "Пути к изображениям успешно заменены!"
