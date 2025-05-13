063a4ad6319d7c8e674c6c77b11a3495785b9f1c
Anna Kapitanova <totoshechka19@gmail.com>
2025-05-13 14:32:55 +0300

    поправила стили плюсика

--- src/components/admin/products-list.tsx
+++ src/components/admin/products-list.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../../store/store';
 import styles from '../../styles/admin/products-list.module.scss';
